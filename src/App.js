4b0e53c6f5c0e88680e2d3b7683acf28ac18a9a3
Nkwitchoua <ali.dias.0000@gmail.com>
2022-07-05 21:09:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import AllContextProvider from './context/AllContext';
+import AboutUs from './pages/AboutUs';
+import AllPosts from './pages/AllPosts';
+import AllVideos from './pages/AllVideos';
+import Contacts from './pages/Contacts';
+import CurrentPost from './pages/CurrentPost';
+import CurrentVideo from './pages/CurrentVideo';
+import MainPage from './pages/MainPage';
 
 function App() {
+
+  // axios.get("http://51.250.39.117:8000/videos/10").then(res => console.log(res.data)).then(data => console.log(data, "DATA"))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AllContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainPage/>} path="/" />
+          <Route element={<AllVideos/>} path="/blog/videos" />
+          <Route element={<AllPosts/>} path="/blog/posts" />
+          <Route element={<AboutUs/>} path="/about-us" />
+          <Route element={<Contacts/>} path="/contacts" />
+          <Route element={<CurrentVideo/>} path="/blog/videos/:videoId" />
+          <Route element={<CurrentPost/>} path="/blog/posts/:postId" />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </AllContextProvider>
   );
 }
 
